463db39b3fb40f7c0914754007b8cf0eb0e98e12
Mark Stemm <mark.stemm@gmail.com>
2021-10-08 16:54:29 -0700

    Move async field extraction from framework to plugin-sdk-go
    Move async field extraction from the framework to plugin-sdk-go:
    
     - wrappers.c now contains a C implementation of the code that used to
        be in the C++ class sinsp_async_extractor. The public functions
        are:
        - create_async_extractor()
        - destroy_async_extractor()
        - async_extractor_wait()
    
       The C function to call to extract a value is
       async_extractor_extract_field(), which coordinates with the
       goroutine using the async_extractor_info struct.
    
     - To enable async extraction, a plugin should call
       RegisterAsyncExtractors as before (minus the async_extractor_info
       struct argument). It now calls create_async_extractor().
    
     - The goroutine created in RegisterAsyncExtractors() now calls
       async_extractor_wait() to wait instead of a wait function pointer
       in the sturct.
    
     - A new wrapper RegisterExtractors() *must* be called by the
       plugin. It's used by plugin_extract_fields_sync() to call the
       simpler go-only extract functions as needed.
    
     - plugin_extract_fields() is now implemented in C. When async
       extraction is enabled, it calls async_extractor_extract_field(). When
       it is not enabled, it calls plugin_extract_fields_sync().
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

---
+++ wrappers/wrappers.h
@@ -0,0 +1,60 @@
+/*
+Copyright (C) 2021 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef void ss_plugin_t;
+
+// XXX/mstemm find a way to include plugin_info.h to avoid duplicating
+// these types/values.
+
+#define SS_PLUGIN_SUCCESS 0
+
+typedef struct ss_plugin_event
+{
+	uint64_t evtnum;
+	uint8_t *data;
+	uint32_t datalen;
+	uint64_t ts;
+} ss_plugin_event;
+
+typedef struct ss_plugin_extract_field
+{
+	const char *field;
+	const char *arg;
+	uint32_t ftype;
+
+	bool field_present;
+	char *res_str;
+	uint64_t res_u64;
+} ss_plugin_extract_field;
+
+typedef struct async_extractor_info
+{
+	// Pointer as this allows swapping out events from other
+	// structs.
+	const ss_plugin_event *evt;
+	ss_plugin_extract_field *field;
+	int32_t rc;
+	int lock;
+} async_extractor_info;
+
+async_extractor_info * create_async_extractor();
+void destroy_async_extractor();
+bool async_extractor_wait(async_extractor_info *ainfo);
