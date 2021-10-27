8f2c3ef8bc2fdbcab755981bf737415003836f4f
Jason Dellaluce <jasondellaluce@gmail.com>
2021-10-27 10:24:44 +0000

    new(examples): introducing example of extractor plugin
    Co-authored-by: Leonardo Grasso <me@leonardograsso.com>
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

---
+++ examples/extractor/extractor.go
@@ -0,0 +1,75 @@
+/*
+Copyright (C) 2021 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/extractor"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/extract"
+)
+
+type MyPlugin struct {
+	plugins.BasePlugin
+}
+
+func init() {
+	extractor.Register(&MyPlugin{})
+}
+
+func (m *MyPlugin) Info() *plugins.Info {
+	return &plugins.Info{
+		ID:                  999,
+		Name:                "extractor-example",
+		Description:         "An Extractor Plugin Example",
+		Contact:             "github.com/falcosecurity/plugin-sdk-go/",
+		Version:             "0.1.0",
+		RequiredAPIVersion:  "0.2.0",
+		ExtractEventSources: []string{"example"},
+	}
+}
+
+func (m *MyPlugin) Init(config string) error {
+	extract.StartAsync(m)
+	return nil
+}
+
+// (optional)
+func (m *MyPlugin) Destroy() {
+	extract.StopAsync(m)
+}
+
+func (m *MyPlugin) Fields() []sdk.FieldEntry {
+	return []sdk.FieldEntry{
+		{Type: "uint64", Name: "example.ts", Display: "", Desc: "The current timestamp"},
+	}
+}
+
+func (m *MyPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
+	switch req.Field() {
+	case "example.ts":
+		req.SetU64Value(uint64(time.Now().UnixNano()))
+		return nil
+	default:
+		return fmt.Errorf("[extractor-example] unsupported field: %s", req.Field())
+	}
+}
+
+func main() {}
