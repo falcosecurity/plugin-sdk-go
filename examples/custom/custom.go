05e7f9640cf511ec4cba36a31498863ba4e90f35
Jason Dellaluce <jasondellaluce@gmail.com>
2021-10-28 16:36:30 +0000

    docs(examples): cleanup examples and update the docs
    Co-authored-by: Leonardo Grasso <me@leonardograsso.com>
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- examples/custom/custom.go
+++ examples/custom/custom.go
@@ -14,17 +14,40 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This plugin is an advanced example of source plugin with the optional
+// extraction capabilities. Unlike the other examples, this does not make use
+// on the high-level constructs of the sdk/plugins package, but instead
+// it directly uses the low-level sdk/symbols packages. This approach is
+// more complex and generally discouraged, but it can be useful in case a
+// plugin developer has the need of defining an exported C symbol of the
+// framework manually.
+//
+// The plugin produces events of the "example" data source containing
+// a simple "hello world" string. The plugin is capable of extracting the
+// "example.hello" field from the "example" event source, which just retrieves
+// the hello world string written in the event.
 package main
 
+/*
+#include <stdint.h>
+*/
+import "C"
+
+// Importing "C" is necessary to include CGO in the plugin and export C symbols.
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"time"
+	"unsafe"
 
+	// Each one of these imported package provide a SDK prebuilt implementation
+	// of some exported C symbols needed by the framework. The _ notation near
+	// some inputs is necessary to avoid Go linters to remove the package if
+	// unused.
+	"github.com/falcosecurity/plugin-sdk-go/pkg/cgo"
+	"github.com/falcosecurity/plugin-sdk-go/pkg/ptr"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
-	_ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/evtstr"
 	_ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/extract"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/fields"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/info"
@@ -32,20 +55,44 @@ import (
 	_ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/lasterr"
 	_ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/nextbatch"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/open"
+	// _ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/evtstr"
 )
 
+// Defining a type for the plugin.
+// Composing the struct with plugins.Base* is the recommended practice
+// as it provides the boilerplate code that satisfies most of the interface
+// requirements of the SDK. Each Base struct implement some interface methods
+// required by the prebuilt C symbols of the SDK. Each symbol package documents
+// which interface it expects to be implemented. Generally, not implementing
+// a required interface leads to a panic at runtime.
+//
+// State variables to store in the plugin must be defined here.
+// In this simple example, we store the configuration string passed by the
+// SDK during the plugin initialization.
 type MyPlugin struct {
 	plugins.BaseLastError
 	plugins.BaseExtractRequests
 	plugins.BaseStringer
 }
 
+// Defining a type for the plugin source capture instances returned by Open().
+// Multiple instances of the same plugin can be opened at the same time for
+// different capture sessions.
+// Composing the struct with plugins.BaseInstance is the recommended practice
+// as it provides the boilerplate code that satisfies most of the interface
+// requirements of the SDK.
+//
+// State variables to store in each plugin instance must be defined here.
 type MyInstance struct {
 	plugins.BaseEvents
 	plugins.BaseProgress
 }
 
+// The plugin information must be initialized in the SDK in the init()
+// function.
 func init() {
+	// Set the general plugin information one by one, also including
+	// its type (source or extractor).
 	info.SetId(999)
 	info.SetName("custom-example")
 	info.SetDescription("A Source Plugin Example")
@@ -55,30 +102,73 @@ func init() {
 	info.SetType(sdk.TypeSourcePlugin)
 	info.SetEventSource("example")
 
+	// Define an initialization callback
 	initialize.SetOnInit(OnInit)
+
+	// Define a callback for opening the event source.
 	open.SetOnOpen(OnOpen)
 
+	// Setting the fields supported for extraction.
 	fields.SetFields([]sdk.FieldEntry{
 		{Type: "string", Name: "example.hello", Display: "Hello World", Desc: "An hello world string"},
 	})
 }
 
+// OnInit is a function that is called by the prebuilt plugin_init C symbol
+// to initialize the plugin and return a plugin state.
 func OnInit(config string) (sdk.PluginState, error) {
 	return &MyPlugin{}, nil
 }
 
+// OnOpen is a function that is called by the prebuilt plugin_open C symbol
+// to open the plugin event source and return a plugin instance state.
 func OnOpen(params string) (sdk.InstanceState, error) {
 	return &MyInstance{}, nil
 }
 
-func (i *MyPlugin) String(in io.ReadSeeker) (string, error) {
-	b, err := ioutil.ReadAll(in)
+// We provide a custom implementation of the plugin_event_to_string C symbol
+// required by the framework, not using the prebuilt one provided by the SDK.
+// The prebuilt one could be used by adding the following to the import list:
+// 	_ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/evtstr"
+// This implementation mimics the one of the prebuilt symbol. The purpose of
+// this example is to show how to define custom C symbols to make them fit
+// with the other SDk prebuilt symbols system.
+//
+//export plugin_event_to_string
+func plugin_event_to_string(pState C.uintptr_t, data *C.uint8_t, datalen uint32) *C.char {
+	// The prebuilt SDK symbols store the plugin state as a handle of the
+	// SDK cgo.Handle. As such, using it is required to be compliant with the
+	// other imported prebuilt symbols.
+	pHandle := cgo.Handle(pState)
+
+	// Our plugin state has a reusable buffer for the event_to_string method
+	buffer := pHandle.Value().(sdk.StringerBuffer).StringerBuffer()
+
+	// We use ptr.BytesReadWriter to safely accessing C-allocated memory
+	bytesReader, err := ptr.NewBytesReadWriter(unsafe.Pointer(data), int64(datalen), int64(datalen))
 	if err != nil {
-		return "", err
+		buffer.Write(err.Error())
+	} else {
+		// Read the string written in the event data using io funtions
+		bytes, err := ioutil.ReadAll(bytesReader)
+		if err != nil {
+			buffer.Write(err.Error())
+		} else {
+			// Set the string as return value by writing it in the
+			// reusable buffer
+			buffer.Write(string(bytes))
+		}
 	}
-	return string(b), nil
+
+	// Extract a char* pointer from the reusable buffer and use it as
+	// the return value.
+	return (*C.char)(buffer.CharPtr())
 }
 
+// Next produces a single new event, and is called repeatedly by the framework.
+// For source plugins, it's mandatory to specify either a Next or a NextBatch
+// method. If both are specified, the prebuilt plugin_next_batch C symbol will
+// ignore the Next method and will only produce new events with NextBatch.
 func (i *MyInstance) Next(pState sdk.PluginState, evt sdk.EventWriter) error {
 	writer := evt.Writer()
 	if _, err := writer.Write([]byte("hello world")); err != nil {
@@ -88,6 +178,9 @@ func (i *MyInstance) Next(pState sdk.PluginState, evt sdk.EventWriter) error {
 	return nil
 }
 
+// This method is optional for source plugins, and enables the extraction
+// capabilities. This is required and called by the prebuilt
+// plugin_extract_fields C symbol.
 func (p *MyPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
 	bytes, err := ioutil.ReadAll(evt.Reader())
 	if err != nil {
@@ -104,17 +197,35 @@ func (p *MyPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
 	return nil
 }
 
-// // (optional)
+// NextBatch produces a batch of new events, and is called repeatedly by the
+// framework. For source plugins, it's mandatory to specify either a Next or a
+// NextBatch method. If both are specified, the prebuilt plugin_next_batch C symbol will
+// ignore the Next method and will only produce new events with NextBatch.
+// The batch has a maximum size that dependes on the size of the underlying
+// reusable memory buffer. A batch can be smaller than the maximum size.
 // func (i *instanceState) NextBatch(pState sdk.PluginState, evts sdk.EventWriters) error {
 //
 // }
 
-// // (optional)
+// Progress returns a percentage indicator referring to the production progress
+// of the event source of this plugin. The Progress method is required if using
+// the prebuilt plugin_get_progress C symbol, which we omitted in this example.
+// The symbol can be included by adding the following to the import list:
+// import _ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/progress"
+// func (m *MyInstance) Progress(pState sdk.PluginState) (float64, string) {
+//
+// }
+
+// Close is gets called by the prebuilt plugin_close C symbol when the plugin
+// source capture gets closed. This is useful to release any open resource used
+// by each plugin instance.
 // func (p *instanceState) Close() {
 //
 // }
 
-// // (optional)
+// Destroy is gets called by the prebuilt plugin_destroy C symbol when the
+// plugin gets deinitialized. This is useful to release any open resource
+// used by the plugin.
 // func (p *MyPlugin) Destroy() {
 //
 // }
--- examples/full/full.go
+++ examples/full/full.go
@@ -14,6 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This plugin is a simple example of source plugin with the optional
+// extraction capabilities.
+// The plugin produces events of the "example" data source containing
+// a single uint64 representing the incrementing value of a counter,
+// serialized using a encoding/gob encoder. The plugin is capable of
+// extracting the "example.count" and "example.countstr" fields from the
+// "example" event source, which are simple numeric and string representations
+// of the counter value.
 package main
 
 import (
@@ -28,21 +36,48 @@ import (
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
 )
 
+// Defining a type for the plugin.
+// Composing the struct with plugins.BasePlugin is the recommended practice
+// as it provides the boilerplate code that satisfies most of the interface
+// requirements of the SDK.
+//
+// State variables to store in the plugin must be defined here.
+// In this simple example, we store the configuration string passed by the
+// SDK during the plugin initialization.
 type MyPlugin struct {
 	plugins.BasePlugin
 }
 
+// Defining a type for the plugin source capture instances returned by Open().
+// Multiple instances of the same plugin can be opened at the same time for
+// different capture sessions.
+// Composing the struct with plugins.BaseInstance is the recommended practice
+// as it provides the boilerplate code that satisfies most of the interface
+// requirements of the SDK.
+//
+// State variables to store in each plugin instance must be defined here.
+// In this example, we store the internal value of the incrementing counter.
 type MyInstance struct {
 	source.BaseInstance
 	counter uint64
 }
 
+// The plugin must be registered to the SDK in the init() function.
+// Registering the plugin using both source.Register and extractor.Register
+// declares to the SDK a source plugin with the optional extraction features
+// enabled. The order in which the two Register functions are called is not
+// relevant, as the SDK induces that the registered plugin is a source plugin.
+// This requires our plugin to implement the source.Plugin interface, so
+// compilation will fail if the mandatory methods are not implemented.
 func init() {
 	p := &MyPlugin{}
 	extractor.Register(p)
 	source.Register(p)
 }
 
+// Info returns a pointer to a plugin.Info struct, containing all the
+// general information about this plugin.
+// This method is mandatory for source plugins.
 func (m *MyPlugin) Info() *plugins.Info {
 	return &plugins.Info{
 		ID:                 999,
@@ -55,10 +90,16 @@ func (m *MyPlugin) Info() *plugins.Info {
 	}
 }
 
+// Init initializes this plugin with a given config string, which is unused
+// in this example. This method is mandatory for source plugins.
 func (m *MyPlugin) Init(config string) error {
 	return nil
 }
 
+// Fields return the list of extractor fields exported by this plugin.
+// This method is optional for source plugins, and enables the extraction
+// capabilities. If the Fields method is defined, the framework expects
+// an Extract method to be specified too.
 func (m *MyPlugin) Fields() []sdk.FieldEntry {
 	return []sdk.FieldEntry{
 		{Type: "uint64", Name: "example.count", Display: "Counter value", Desc: "Current value of the internal counter"},
@@ -66,6 +107,9 @@ func (m *MyPlugin) Fields() []sdk.FieldEntry {
 	}
 }
 
+// This method is optional for source plugins, and enables the extraction
+// capabilities. If the Extract method is defined, the framework expects
+// an Fields method to be specified too.
 func (m *MyPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
 	var value uint64
 	encoder := gob.NewDecoder(evt.Reader())
@@ -85,12 +129,31 @@ func (m *MyPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error {
 	}
 }
 
+// Open opens the plugin source and starts a new capture session (e.g. stream
+// of events), creating a new plugin instance. The state of each instance can
+// be initialized here. This method is mandatory for source plugins.
 func (m *MyPlugin) Open(params string) (source.Instance, error) {
-	return &MyInstance{
+	// An event batch buffer can optionally be defined to specify custom
+	// values for max data size or max batch size. If nothing is set
+	// with the SetEvents method, the SDK will provide a default value
+	// after the Open method returns.
+	// In this example, we want to allocate a batch of max 10 events, each
+	// one of max 64 bytes, which is more than enough to host the serialized
+	// incrementing counter value.
+	myBatch, err := sdk.NewEventWriters(10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	myInstance := &MyInstance{
 		counter: 0,
-	}, nil
+	}
+	myInstance.SetEvents(myBatch)
+	return myInstance, nil
 }
 
+// String produces a string representation of an event data produced by the
+// event source of this plugin. This method is mandatory for source plugins.
 func (m *MyPlugin) String(in io.ReadSeeker) (string, error) {
 	var value uint64
 	encoder := gob.NewDecoder(in)
@@ -100,32 +163,54 @@ func (m *MyPlugin) String(in io.ReadSeeker) (string, error) {
 	return fmt.Sprintf("counter: %d", value), nil
 }
 
-func (m *MyInstance) Next(pState sdk.PluginState, evt sdk.EventWriter) error {
-	m.counter++
-	encoder := gob.NewEncoder(evt.Writer())
-	if err := encoder.Encode(m.counter); err != nil {
-		return err
+// NextBatch produces a batch of new events, and is called repeatedly by the
+// framework. For source plugins, it's mandatory to specify either a Next or a
+// NextBatch method. If both are specified, the SDK will ignore the Next method
+// and will only produce new events with NextBatch.
+// The batch has a maximum size that dependes on the size of the underlying
+// reusable memory buffer. A batch can be smaller than the maximum size.
+func (m *MyInstance) NextBatch(pState sdk.PluginState, evts sdk.EventWriters) (int, error) {
+	var n int
+	var evt sdk.EventWriter
+	for n = 0; n < evts.Len(); n++ {
+		evt = evts.Get(n)
+		m.counter++
+		encoder := gob.NewEncoder(evt.Writer())
+		if err := encoder.Encode(m.counter); err != nil {
+			return 0, err
+		}
+		evt.SetTimestamp(uint64(time.Now().UnixNano()))
 	}
-	evt.SetTimestamp(uint64(time.Now().UnixNano()))
-	return nil
+	return n, nil
 }
 
-// // (optional)
-// func (m *MyInstance) NextBatch(pState sdk.PluginState, evts sdk.EventWriters) (int, error) {
+// Next produces a single new event, and is called repeatedly by the framework.
+// For source plugins, it's mandatory to specify either a Next or a NextBatch
+// method. If both are specified, the SDK will ignore the Next method and will
+// only produce new events with NextBatch.
+// func (m *MyInstance) Next(pState sdk.PluginState, evt sdk.EventWriter) error {
 //
 // }
 
-// // (optional: requires import _ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/progress)"
+// Progress returns a percentage indicator referring to the production progress
+// of the event source of this plugin.
+// This method is optional for source plugins. If specified, the following
+// package needs to be imported to advise the SDK to enable this feature:
+// import _ "github.com/falcosecurity/plugin-sdk-go/pkg/sdk/symbols/progress"
 // func (m *MyInstance) Progress(pState sdk.PluginState) (float64, string) {
 //
 // }
 
-// // (optional)
+// Close is gets called by the SDK when the plugin source capture gets closed.
+// This is useful to release any open resource used by each plugin instance.
+// This method is optional for source plugins.
 // func (m *MyInstance) Close() {
 //
 // }
 
-// // (optional)
+// Destroy is gets called by the SDK when the plugin gets deinitialized.
+// This is useful to release any open resource used by the plugin.
+// This method is optional for source plugins.
 // func (m *MyPlugin) Destroy() {
 //
 // }
