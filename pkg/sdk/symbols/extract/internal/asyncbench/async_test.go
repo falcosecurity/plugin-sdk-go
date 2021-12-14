cc8e1968a8a80409f1c9ab35e59faa5926375dda
Leonardo Grasso <me@leonardograsso.com>
2021-12-14 16:51:59 +0100

    update(sdk/symbols): add benchmark suite for async extractor
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

---
+++ pkg/sdk/symbols/extract/internal/asyncbench/async_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright (C) 2021 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package asyncbench
+
+import (
+	"testing"
+)
+
+func TestWorker(t *testing.T) {
+	var lock int32
+	startWorker(&lock)
+
+	inData = testInput
+	dataRequest(&lock)
+	if outData != expectedOut {
+		t.Fatalf(`dataRequest failed: expected "%d", got "%d"`, expectedOut, outData)
+	}
+
+	exitRequest(&lock)
+}
+
+func BenchmarkCall_Go(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = doWork(testInput)
+	}
+}
+
+func BenchmarkCall_C_to_Go(b *testing.B) {
+	benchmark_sync(b.N, testInput)
+}
+
+func BenchmarkCall_Go_to_C(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = do_work_c(testInput)
+	}
+}
+
+func BenchmarkWorker_GoCaller(b *testing.B) {
+	var lock int32
+	startWorker(&lock)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		dataRequest(&lock)
+	}
+	b.StopTimer()
+	exitRequest(&lock)
+}
+
+func BenchmarkWorker_CCaller(b *testing.B) {
+	var lock int32
+	startWorker(&lock)
+	b.ResetTimer()
+	benchmark_async(&lock, b.N)
+	b.StopTimer()
+	exitRequest(&lock)
+}
---
+++ pkg/sdk/symbols/extract/internal/asyncbench/bench.c
@@ -0,0 +1,56 @@
+/*
+Copyright (C) 2021 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "bench.h"
+
+void data_request(atomic_int_least32_t *lock)
+{
+    // We assume state_wait because no concurrent requests are supported
+    enum worker_state old_val = WAIT;
+    while (!atomic_compare_exchange_strong_explicit(lock, (uint32_t *)&old_val, DATA_REQ, memory_order_seq_cst, memory_order_seq_cst))
+    {
+        old_val = WAIT;
+    }
+
+    // state_data_req acquired, wait for worker completation
+    while (atomic_load_explicit(lock, memory_order_seq_cst) != WAIT);
+}
+
+void async_benchmark(int32_t *lock, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        data_request((atomic_int_least32_t *)lock);
+    }
+}
+
+// Defined in async.go
+extern int doWork(int);
+
+int sync_benchmark(int n, int input)
+{
+    int output;
+    for (int i = 0; i < n; i++)
+    {
+        output = doWork(input);
+    }
+    return output;
+}
+
+int do_work_c(int i)
+{
+    return i + 1;
+}
