fc7bf8d67349105c625eb73eed11059aa29ca951
Jason Dellaluce <jasondellaluce@gmail.com>
2022-07-15 16:20:15 +0000

    wip: improve
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- pkg/sdk/symbols/extract/async.go
+++ pkg/sdk/symbols/extract/async.go
@@ -41,27 +41,148 @@ const (
 const (
 	starvationThresholdNs = int64(1e6)
 	sleepTimeNs           = 1e7 * time.Nanosecond
-	// todo(jasondellaluce): change this if we increase the max number of handles
-	asyncCtxSize = cgo.MaxHandle + 1
 )
 
 var (
-	asyncCtx     *C.async_extractor_info
-	asyncMutex   sync.Mutex
-	asyncEnabled bool  = true
-	asyncCount   int32 = 0
+	// asyncEnabled is true if the async optimization is configured as enabled
+	asyncEnabled bool = true
+	//
+	// asyncMutex ensures that StartAsync() and StopAsync are used in mutual
+	// exclusion
+	asyncMutex sync.Mutex
+	//
+	// asyncCount is incremented at every call to StartAsync() and
+	// is decremented at every call to StopAsync()
+	asyncCount int32 = 0
+	//
+	// asyncCtxBatch is a slice of async info contexts that is shared between
+	// C and Go. Each batch slot contains a unique lock and is assigned to
+	// only one cgo.Handle value.
+	//
+	// note(jasondellaluce): this is dependent on our internal implementation
+	// of cgo.Handle. Since cgo.Handle values are regular integers between 1
+	// and cgo.MaxHandle, they are used to assign the batch slot index to
+	// each consumer. Each initialized plugin is a distinct consumer and has
+	// its own assigned cgo.Handle value.
+	// I don't like leaking this implementation knowledge of our cgo package,
+	// however the goal here is to achieve maximum performance and using
+	// array-based access is the best we can get.
+	//
+	// todo(jasondellaluce): change this if we change cgo.Handle implementation
+	asyncCtxBatch []C.async_extractor_info
+	//
+	// asyncCtxBatchCap is the physical size of asyncCtxBatch as allocated
+	// in C memory, namely the total number of slots available for consumers.
+	asyncCtxBatchCap = cgo.MaxHandle + 1
+	//
+	// asyncCtxBatchLen is the number of occupied slots of asyncCtxBatch. This
+	// value is >= 0 and < asyncCtxBatchCap. The solely purpose of
+	// counting the number of occupied lock slots is for the worker to avoid
+	// looping over the whole batch when only few slots are really used, which
+	// is a big improvement in performance. This value is incremented at every
+	// call to StartAsync(), and is never decremented. The reason is that the
+	// value spaces for cgo.Handle can become sparse when a plugin is destroyed
+	// and its cgo.Handle deleted. We can assume already-assigned cgo.Handles
+	// to be reuased after deletion, but we can't make assumptions on which
+	// is the largest cgo.Handle used in a given moment.
+	asyncCtxBatchLen = uint32(0)
 )
 
-func asyncAvailable() bool {
-	return runtime.NumCPU() > 1
-}
-
+// SetAsync enables or disables the async extraction optimization depending
+// on the passed-in boolean. Note, setting the optimization as enabled does not
+// guarantee that it will be used at runtime: the optimization will not
+// be activated if the hardware configuration doesn't match the minimum requirements.
 func SetAsync(enable bool) {
 	asyncEnabled = enable
 }
 
+// Async returns true if the async extraction optimization is
+// configured as enabled, and false otherwise.
 func Async() bool {
 	return asyncEnabled
+}
+
+// since the async worker can potentially keep a whole thread (and its CPU)
+// busy on high workloads, we consider the optimization available only if
+// we run on at least 2 CPUs.
+func asyncAvailable() bool {
+	// note: runtime.GOMAXPROCS(0) should be more accurate than runtime.NumCPU()
+	// and and better aligns with the developer/user thread pool configuration.
+	//
+	// todo(jasondellaluce): changing runtime.GOMAXPROCS after starting the
+	// async worker would break the plugin
+	return runtime.GOMAXPROCS(0) > 1
+}
+
+func initAsyncCtxBatch() {
+	// initialize the batch in C memory
+	batch := unsafe.Pointer(C.async_init((C.size_t)(asyncCtxBatchCap)))
+
+	// convert the batch into a Go slice
+	(*reflect.SliceHeader)(unsafe.Pointer(&asyncCtxBatch)).Data = uintptr(batch)
+	(*reflect.SliceHeader)(unsafe.Pointer(&asyncCtxBatch)).Len = int(asyncCtxBatchCap)
+	(*reflect.SliceHeader)(unsafe.Pointer(&asyncCtxBatch)).Cap = int(asyncCtxBatchCap)
+
+	// initialize all the locks in the batch
+	for i := 0; i < asyncCtxBatchCap; i++ {
+		atomic.StoreInt32((*int32)(&asyncCtxBatch[i].lock), state_wait)
+	}
+}
+
+func destroyAsyncCtxBatch() {
+	asyncCtxBatch = nil
+	atomic.StoreUint32(&asyncCtxBatchLen, 0)
+	C.async_deinit()
+}
+
+func asyncWorker() {
+	waitStartTime := time.Now().UnixNano()
+	for {
+		// check async context slots in round-robin
+		for i := uint32(1); i < atomic.LoadUint32(&asyncCtxBatchLen)+1; i++ {
+			// Check for incoming request, if any, otherwise busy waits
+			switch atomic.LoadInt32((*int32)(&asyncCtxBatch[i].lock)) {
+
+			case state_data_req:
+				// Incoming data request. Process it...
+				asyncCtxBatch[i].rc = C.int32_t(
+					plugin_extract_fields_sync(
+						C.uintptr_t(uintptr(asyncCtxBatch[i].s)),
+						asyncCtxBatch[i].evt,
+						uint32(asyncCtxBatch[i].num_fields),
+						asyncCtxBatch[i].fields,
+					),
+				)
+				// Processing done, return back to waiting state
+				atomic.StoreInt32((*int32)(&asyncCtxBatch[i].lock), state_wait)
+				// Reset waiting start time
+				waitStartTime = 0
+
+			case state_exit_req:
+				// Incoming exit request. Send ack and exit.
+				atomic.StoreInt32((*int32)(&asyncCtxBatch[i].lock), state_exit_ack)
+				return
+			}
+
+			// busy wait, then sleep after 1ms
+			if waitStartTime == 0 {
+				waitStartTime = time.Now().UnixNano()
+			} else if time.Now().UnixNano()-waitStartTime > starvationThresholdNs {
+				time.Sleep(sleepTimeNs)
+			}
+		}
+	}
+}
+
+func stopAsync() {
+	for !atomic.CompareAndSwapInt32((*int32)(&asyncCtxBatch[1].lock), state_wait, state_exit_req) {
+		// spin
+	}
+
+	// state_exit_req acquired, wait for worker exiting
+	for atomic.LoadInt32((*int32)(&asyncCtxBatch[1].lock)) != state_exit_ack {
+		// spin
+	}
 }
 
 // StartAsync initializes and starts the asynchronous extraction mode.
@@ -89,56 +210,13 @@ func StartAsync() {
 	defer asyncMutex.Unlock()
 
 	asyncCount += 1
+	atomic.AddUint32(&asyncCtxBatchLen, 1)
 	if !asyncAvailable() || !asyncEnabled || asyncCount > 1 {
 		return
 	}
 
-	// initialize async context slots
-	var asyncCtx []C.async_extractor_info
-	(*reflect.SliceHeader)(unsafe.Pointer(&asyncCtx)).Data = uintptr(unsafe.Pointer(C.async_init((C.size_t)(asyncCtxSize))))
-	(*reflect.SliceHeader)(unsafe.Pointer(&asyncCtx)).Len = int(asyncCtxSize)
-	(*reflect.SliceHeader)(unsafe.Pointer(&asyncCtx)).Cap = int(asyncCtxSize)
-	for i := 0; i < asyncCtxSize; i++ {
-		atomic.StoreInt32((*int32)(&asyncCtx[i].lock), state_wait)
-	}
-	go func() {
-		waitStartTime := time.Now().UnixNano()
-		for {
-			// check async context slots in round-robin
-			for i := 0; i < asyncCtxSize; i++ {
-				// Check for incoming request, if any, otherwise busy waits
-				switch atomic.LoadInt32((*int32)(&asyncCtx[i].lock)) {
-
-				case state_data_req:
-					// Incoming data request. Process it...
-					asyncCtx[i].rc = C.int32_t(
-						plugin_extract_fields_sync(
-							C.uintptr_t(uintptr(asyncCtx[i].s)),
-							asyncCtx[i].evt,
-							uint32(asyncCtx[i].num_fields),
-							asyncCtx[i].fields,
-						),
-					)
-					// Processing done, return back to waiting state
-					atomic.StoreInt32((*int32)(&asyncCtx[i].lock), state_wait)
-					// Reset waiting start time
-					waitStartTime = 0
-
-				case state_exit_req:
-					// Incoming exit request. Send ack and exit.
-					atomic.StoreInt32((*int32)(&asyncCtx[i].lock), state_exit_ack)
-					return
-				}
-
-				// busy wait, then sleep after 1ms
-				if waitStartTime == 0 {
-					waitStartTime = time.Now().UnixNano()
-				} else if time.Now().UnixNano()-waitStartTime > starvationThresholdNs {
-					time.Sleep(sleepTimeNs)
-				}
-			}
-		}
-	}()
+	initAsyncCtxBatch()
+	go asyncWorker()
 }
 
 // StopAsync deinitializes and stops the asynchronous extraction mode, and
@@ -153,18 +231,8 @@ func StopAsync() {
 		panic("plugin-sdk-go/sdk/symbols/extract: async worker stopped without being started")
 	}
 
-	if asyncCount == 0 && asyncCtx != nil {
-		lock := (*int32)(&asyncCtx.lock)
-
-		for !atomic.CompareAndSwapInt32(lock, state_wait, state_exit_req) {
-			// spin
-		}
-
-		// state_exit_req acquired, wait for worker exiting
-		for atomic.LoadInt32(lock) != state_exit_ack {
-			// spin
-		}
-		asyncCtx = nil
-		C.async_deinit()
+	if asyncCount == 0 && asyncCtxBatch != nil {
+		stopAsync()
+		destroyAsyncCtxBatch()
 	}
 }
