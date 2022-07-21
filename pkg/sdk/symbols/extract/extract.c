81cdd3873bbca937f4787ed7d02fb7357fe33026
Jason Dellaluce <jasondellaluce@gmail.com>
2022-07-21 10:52:26 +0000

    refactor(pkg/sdk/symbols/extract): adapt C-side async request
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- pkg/sdk/symbols/extract/extract.c
+++ pkg/sdk/symbols/extract/extract.c
@@ -35,18 +35,18 @@ enum worker_state
 	EXIT_ACK = 3,
 };
 
-static async_extractor_info *s_async_extractor_ctx = NULL;
+static async_extractor_info *s_async_ctx_batch = NULL;
 
 async_extractor_info *async_init(size_t size)
 {
-	s_async_extractor_ctx = (async_extractor_info *)malloc(sizeof(async_extractor_info) * size);
-	return s_async_extractor_ctx;
+	s_async_ctx_batch = (async_extractor_info *)malloc(sizeof(async_extractor_info) * size);
+	return s_async_ctx_batch;
 }
 
 void async_deinit()
 {
-	free(s_async_extractor_ctx);
-	s_async_extractor_ctx = NULL;
+	free(s_async_ctx_batch);
+	s_async_ctx_batch = NULL;
 }
 
 // Defined in extract.go
@@ -60,25 +60,32 @@ static inline int32_t async_extract_request(ss_plugin_t *s,
 											uint32_t num_fields,
 											ss_plugin_extract_field *fields)
 {
-	// Since no concurrent requests are supported,
-	// we assume worker is already in WAIT state
+	// note: concurrent requests are supported on the context batch, but each
+	// slot with a different value of ss_plugin_t *s. As such, for each lock
+	// we assume worker is already in WAIT state. This is possible because
+	// ss_plugin_t *s is an integer number representing a cgo.Handle, and can
+	// have values in the range of [1, cgo.MaxHandle]
+	//
+	// todo(jasondellaluce): this is dependent on the implementation of our
+	// cgo.Handle to optimize performance, so change this if we ever change
+	// how cgo.Handles are represented
 
 	// Set input data
-	s_async_extractor_ctx->s = s;
-	s_async_extractor_ctx->evt = evt;
-	s_async_extractor_ctx->num_fields = num_fields;
-	s_async_extractor_ctx->fields = fields;
+	s_async_ctx_batch[(size_t)s - 1].s = s;
+	s_async_ctx_batch[(size_t)s - 1].evt = evt;
+	s_async_ctx_batch[(size_t)s - 1].num_fields = num_fields;
+	s_async_ctx_batch[(size_t)s - 1].fields = fields;
 
 	// notify data request
-	atomic_store_explicit(&s_async_extractor_ctx->lock, DATA_REQ, memory_order_seq_cst);
+	atomic_store_explicit(&s_async_ctx_batch[(size_t)s - 1].lock, DATA_REQ, memory_order_seq_cst);
 
 	// busy-wait until worker completation
-	while (atomic_load_explicit(&s_async_extractor_ctx->lock, memory_order_seq_cst) != WAIT);
+	while (atomic_load_explicit(&s_async_ctx_batch[(size_t)s - 1].lock, memory_order_seq_cst) != WAIT);
 
-	return s_async_extractor_ctx->rc;
+	return s_async_ctx_batch[(size_t)s - 1].rc;
 }
 
-// This is the plugin API function. If s_async_extractor_ctx is
+// This is the plugin API function. If s_async_ctx_batch is
 // non-NULL, it calls the async extractor function. Otherwise, it
 // calls the synchronous extractor function.
 FALCO_PLUGIN_SDK_PUBLIC int32_t plugin_extract_fields(ss_plugin_t *s,
@@ -86,7 +93,7 @@ FALCO_PLUGIN_SDK_PUBLIC int32_t plugin_extract_fields(ss_plugin_t *s,
 							  uint32_t num_fields,
 							  ss_plugin_extract_field *fields)
 {
-	if (s_async_extractor_ctx != NULL)
+	if (s_async_ctx_batch != NULL)
 	{
 		return async_extract_request(s, evt, num_fields, fields);
 	}
